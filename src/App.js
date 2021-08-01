a5834db049ec879dae7a86d61d53b4031c5ba3d5
yehnet <yehnet@post.bgu.ac.il>
2021-08-01 12:57:35 +0300

    Components playground

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React, {Component} from "react";
+import HighScore from "./HighScore";
+import './css/App.css'
 
-class App extends Component{
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-    render(){
-        return 'Hello World!';
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }
+
+    handleClicks = () => {
+        this.setState({count: this.state.count + 1})
+        // console.log("Clicked");
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // console.log("Update from", prevState, "to", this.state)
+        if (this.state.count > 10 && !this.state.overTen) {
+            console.log("Updating overTen.");
+            this.setState({overTen: true});
+        }
+    }
+
+    resetCount = (e) => {
+        console.log("Event is", e);
+        this.setState({
+            count: 0,
+            overTen: false
+        });
+    }
+
+    render() {
+        let {count} = this.state;
+
+        return (
+            <div>
+                <h1>You clicked the button {count} times!</h1>
+                <HighScore
+                    overTen={this.state.overTen}
+                    onReset={this.resetCount}
+                />
+                <span>
+                        <button onClick={() => this.handleClicks()}>Click Me</button>
+                        </span>
+            </div>
+        );
     }
 }
 
