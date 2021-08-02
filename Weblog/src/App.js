b4b500b57bbc4f9418808d609444aee55030947a
yehnet <yehnet@post.bgu.ac.il>
2021-08-02 12:29:03 +0300

    HTML init

--- Weblog/src/App.js
+++ Weblog/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import PageWrapper from "./Components/PageWrapper";
+import React, {Component} from "react";
+import Home from "./Components/Pages/Home";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+
+                <PageWrapper>
+                    <Home/>
+                </PageWrapper>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
