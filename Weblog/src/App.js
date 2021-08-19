46ede812e2bef70ef07e9792b5196b72005f14fc
yehnet <yehnet@post.bgu.ac.il>
2021-08-19 11:41:57 +0300

    Route parameters + about page

--- Weblog/src/App.js
+++ Weblog/src/App.js
@@ -1,16 +1,31 @@
 import PageWrapper from "./Components/PageWrapper";
 import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Router>
+                    <PageWrapper>
 
-                <PageWrapper>
-                    <Home/>
-                </PageWrapper>
-                <h1>Hello World!</h1>
+                        <Route
+                            exact={true}
+                            path="/"
+                            component={Home}
+                        />
+
+                        <Route
+                            path="/about"
+                            component={About}
+                        />
+
+
+                    </PageWrapper>
+                </Router>
             </div>
         );
     }
--- Weblog/src/Components/Pages/Home.js
+++ Weblog/src/Components/Pages/Home.js
@@ -1,20 +1,21 @@
 import React, {Component} from "react";
+import Header from "../Common/Header";
+
+import headerImg from "../Assets/img/header-bg.jpg";
 
 class Home extends Component {
 
     render() {
         return (
             <div>
-                <header className="masthead">
-                    <div className="container">
-                        <div className="intro-text">
-                            <div className="intro-lead-in">Welcome To Our Studio!</div>
-                            <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
-                            <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell
-                                Me More</a>
-                        </div>
-                    </div>
-                </header>
+                <Header
+                    title="Welcome To Our Studio!"
+                    subtitle="IT's NICE TO MEET YOU"
+                    buttonText="Tell ME MORE"
+                    link="/services"
+                    showButton={true}
+                    image={headerImg}
+                />
             </div>
         )
     }
